128b55e968438931f0df26d8c0b66e294f782c46
Adriel Klein <adrielmklein@gmail.com>
2016-11-03 09:18:09 -0700

    add deck

---
+++ deck.py
@@ -0,0 +1,30 @@
+from random import shuffle
+
+
+class Deck(object):
+    def __init__(self):
+        self.remaining_cards = []
+        self.drawn_cards = []
+
+    def add_card(self, card):
+        self.remaining_cards.append(card)
+
+    def get_num_remaining_cards(self):
+        return len(self.remaining_cards)
+
+    def get_num_drawn_cards(self):
+        return len(self.drawn_cards)
+
+    def draw(self):
+        if self.get_num_remaining_cards() == 0:
+            raise Exception('No cards left to draw. Please reset the deck or add cards.')
+        card = self.remaining_cards.pop()
+        self.drawn_cards.append(card)
+        return card
+
+    def reset(self):
+        self.remaining_cards += self.drawn_cards
+        self.drawn_cards = []
+
+    def shuffle(self):
+        shuffle(self.remaining_cards)
---
+++ test_deck.py
@@ -0,0 +1,55 @@
+import pytest
+
+from deck import Deck
+
+
+def test_when_deck_is_initialized_then_it_is_empty():
+    deck = Deck()
+    assert 0 == deck.get_num_remaining_cards()
+
+
+def test_when_cards_are_added_to_the_deck_then_deck_is_not_empty():
+    deck = Deck()
+    deck.add_card(1)
+    assert 1 == deck.get_num_remaining_cards()
+
+
+def test_when_card_is_added_to_the_deck_and_then_drawn_then_deck_is_empty():
+    deck = Deck()
+    deck.add_card(1)
+    assert 1 == deck.draw()
+    assert 0 == deck.get_num_remaining_cards()
+
+
+def test_when_deck_is_reset_then_remaining_cards_is_reset():
+    deck = Deck()
+    deck.add_card(1)
+    deck.draw()
+    deck.reset()
+    assert 1 == deck.get_num_remaining_cards()
+    assert 0 == deck.get_num_drawn_cards()
+
+
+def test_when_some_cards_have_been_drawn_and_deck_is_reset_then_deck_is_full_again():
+    deck = Deck()
+    deck.add_card(1)
+    deck.add_card(2)
+    deck.draw()
+    deck.reset()
+    assert 2 == deck.get_num_remaining_cards()
+    assert 0 == deck.get_num_drawn_cards()
+
+
+def test_when_deck_is_empty_and_user_draws_card_then_raise_exception():
+    deck = Deck()
+    with pytest.raises(Exception) as excinfo:
+        deck.draw()
+    assert 'No cards left to draw. Please reset the deck or add cards.' == str(excinfo.value)
+
+
+def test_when_deck_is_shuffled_then_cards_get_different_ordering():
+    deck = Deck()
+    for i in range(100):
+        deck.add_card(i)
+    deck.shuffle()
+    assert [i for i in range(100)] != deck.remaining_cards
